efc22d78c7647ce158289ae6cc08ed5fc8e9ebd2
kktkarthikeyan <64134101+kktkarthikeyan@users.noreply.github.com>
2023-10-08 00:15:24 +0530

    Added Mongo Db configuration

--- api/index.js
+++ api/index.js
@@ -1,8 +1,19 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB)
+  .then(() => {
+    console.log("Connected to DB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
-app.listen(3000, ()=>{
-    console.log("server is listening port 3001");
+app.listen(3000, () => {
+  console.log("server is listening port 3001");
 });
-
