0f30eba7ae0a25a1090bab8f48f41e4e6860e424
kktkarthikeyan <64134101+kktkarthikeyan@users.noreply.github.com>
2023-10-08 23:59:43 +0530

    create signup api

---
+++ api/routes/auth.route.js
@@ -0,0 +1,10 @@
+import express from 'express'
+import {signUp} from '../controller/auth.controller.js'
+
+const router = express.Router();
+
+router.post('/signup',signUp);
+
+export default router;
+
+
