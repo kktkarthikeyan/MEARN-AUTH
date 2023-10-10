2f5c31e4223664c6a4f11317063847ada23051a1
kktkarthikeyan <64134101+kktkarthikeyan@users.noreply.github.com>
2023-10-10 22:29:23 +0530

    Signup Page Created

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,6 +1,32 @@
-
 export default function SignUp() {
   return (
-    <div>SignUp</div>
-  )
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7 ">SignUp</h1>
+      <form className="flex flex-col gap-4">
+        <input
+          type="text"
+          placeholder="Username"
+          id="username"
+          className="bg-slate-100 p-3 rounded-lg"
+        ></input>
+        <input
+          type="email"
+          placeholder="Email"
+          id="email"
+          className="bg-slate-100 p-3 rounded-lg"
+        ></input>
+        <input
+          type="password"
+          placeholder="Password"
+          id="password"
+          className="bg-slate-100 p-3 rounded-lg"
+        ></input>
+        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95">Sign Up</button>
+      </form>
+      <div className="flex gap-2 mt-5">
+        <p>Have an account ?</p>
+        <span className="text-blue-500">Sign in</span>
+      </div>
+    </div>
+  );
 }
